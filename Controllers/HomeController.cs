5222d99c05d395936c7fefff483f059d0d58716f
SoloWarrior <karandhir64@yahoo.co.in>
2018-11-16 11:46:59 +0530

    edit functionality

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace TodoListProject.Controllers
         public ActionResult GetTasks()
         {
             List<Task> taskList = null;
-            using (var taskcontext = new TaskContext())
+            using (TaskContext taskcontext = new TaskContext())
             {
                 taskList = taskcontext.Tasks.ToList();
             }
@@ -25,10 +25,10 @@ namespace TodoListProject.Controllers
         [HttpPost]
         public ActionResult AddTask(string todoValue)
         {
-           
-            using (var taskcontext = new TaskContext())
+
+            using (TaskContext taskcontext = new TaskContext())
             {
-                var taskvalue = new Task() { value = todoValue };
+                Task taskvalue = new Task() { value = todoValue };
                 taskcontext.Tasks.Add(taskvalue);
                 taskcontext.SaveChanges();
             }
@@ -37,20 +37,37 @@ namespace TodoListProject.Controllers
         [HttpPost]
         public ActionResult DeleteTask(int id)
         {
-            using (var taskcontext = new TaskContext())
+            using (TaskContext taskcontext = new TaskContext())
             {
-                var deleteTask = taskcontext.Tasks.Find(id);
+                Task deleteTask = taskcontext.Tasks.Find(id);
                 taskcontext.Tasks.Remove(deleteTask);
                 taskcontext.SaveChanges();
             }
             return RedirectToAction("GetTasks");
         }
+        [HttpPost]
         public ActionResult EditTask(int id)
         {
-             
-            
-
-            return View();
+            Task editTask;
+            using (TaskContext taskcontext = new TaskContext())
+            {
+                editTask = taskcontext.Tasks.Find(id);
+            }
+            return View(editTask);
+        }
+        [HttpPost]
+        public ActionResult EditTaskHelper(string editValue, string editId)
+        {
+            Task edittask;
+            using (TaskContext taskcontext = new TaskContext())
+            {
+                
+                int id = Convert.ToInt32(editId);
+                edittask = taskcontext.Tasks.Find(id);
+                edittask.value = editValue;
+                taskcontext.SaveChanges();
+            }
+            return RedirectToAction("GetTasks");
         }
     }
 }
